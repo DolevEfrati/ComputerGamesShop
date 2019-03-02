7f070b9667e5fc4219fb4f887296f2887912f568
Melchek Yakov <melchekyg@gmail.com>
2019-03-02 22:59:40 +0200

    webServicw

--- ComputerGamesShop/wwwroot/js/games.js
+++ ComputerGamesShop/wwwroot/js/games.js
@@ -2,6 +2,10 @@
     $("#max").text($(this).val() + '$');
 });
 
+$('.price').each(function (idx, p) {
+    $(p).html(geoplugin_currencyConverter(p.innerText))
+})
+
 function showVal(newVal) {
     document.getElementById("max").innerHTML = newVal;
 }
